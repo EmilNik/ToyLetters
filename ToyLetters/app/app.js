0de922ba9d8493f1a5574a855d76a733b747f9a4
Emil Nik <emil.nik1002@gmail.com>
2015-12-08 20:02:44 +0200

    Updated Home page

--- ToyLetters/app/app.js
+++ ToyLetters/app/app.js
@@ -1,17 +1,18 @@
 (function () {
 	function changeBackgroundImage() {
 		var imagesUrlArr = [
-			'10947455_1053328038016710_2785022104556472984_o',
+			'12191148_1240706979278814_1143590211717976040_o',
 			'12185114_10204546840221495_8763612778038204814_o',
-			'12232672_1239672912715554_3835335110417238063_o'
+			'12232672_1239672912715554_3835335110417238063_o',
+			'11223777_1252754234740755_399494257669225033_o'
 		];
 
 		var count = 0;
-
+		var div = document.getElementById('background-img-div');
+		div.style.backgroundImage = 'url("resources/images/' + imagesUrlArr[count] + '.jpg")';
 		function nextBackground() {
 			count++;
 			count = count % imagesUrlArr.length;
-			var div = document.getElementById('background-img-div');
 			div.style.backgroundImage = 'url("resources/images/' + imagesUrlArr[count] + '.jpg")';
 		}
 		setInterval(nextBackground, 10000);
@@ -21,6 +22,7 @@
 		var div = document.getElementById('background-img-div');
 		div.style.height = window.innerHeight + 'px';
 	};
-	// changeBackgroundImage();
+	changeBackgroundImage();
 	resize();
+	$(window).resize(resize);
 } ());
