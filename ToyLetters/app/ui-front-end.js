1a2ec4638d99948d3d4093247f837cf879fe0998
Emil Nik <emil.nik1002@gmail.com>
2015-12-09 20:15:10 +0200

    Trying to make the nav responsive... didnt work

--- ToyLetters/app/ui-front-end.js
+++ ToyLetters/app/ui-front-end.js
@@ -1,34 +1,46 @@
 (function () {
-	$('#nav-menu').on('click', function() {
-		if($('#nav-menu-button').hasClass('skrollable-between')) {
+	$('#nav-menu').on('click', function () {
+		if ($('#nav-menu-button').hasClass('skrollable-between')) {
 			return;
 		}
-		
-		$('#nav-menu').toggleClass('active');
-		if($('#nav-menu').hasClass('active')){
-			$('#nav-bg').css('background-color', 'rgba(0, 0, 0, 0.5)');
-			$('#nav-bg').css('opacity', '1');
-			$('.nav-ul-li').css('opacity', '1', 'important');
+
+		if ($('.nav-ul').hasClass('is-desktop')) {
+
+			$('#nav-menu').toggleClass('active');
+			if ($('#nav-menu').hasClass('active')) {
+				$('#nav-bg').css('background-color', 'rgba(0, 0, 0, 0.5)');
+				$('#nav-bg').css('opacity', '1');
+				$('.nav-ul-li').css('opacity', '1', 'important');
+			} else {
+				$('#nav-bg').css('background-color', 'transparent');
+				$('#nav-bg').css('opacity', '0');
+				$('.nav-ul-li').css('opacity', 0);
+			}
 		} else {
-			$('#nav-bg').css('background-color', 'transparent');
-			$('#nav-bg').css('opacity', '0');
-			$('.nav-ul-li').css('opacity', 0);
+			$('#nav-menu').toggleClass('active');
+			if ($('#nav-menu').hasClass('active')) {
+				$('ul.nav-ul.is-mobile li>a').css('opacity', 1);
+				$('ul.nav-ul.is-mobile li').css('background-color', 'rgba(0, 0, 0, 0.5)');
+			} else {
+				$('ul.nav-ul.is-mobile li>a').css('opacity', 0);
+				$('ul.nav-ul.is-mobile li').css('background-color', 'transparent');
+			}
 		}
 	});
-	
-	var removeClassActive = function() {
-		for(var i = 0; i < $('.nav-ul-li-active').length; i++) {
-			if($($('.nav-ul-li-active')[i]).hasClass('active')) {
+
+	var removeClassActive = function () {
+		for (var i = 0; i < $('.nav-ul-li-active').length; i++) {
+			if ($($('.nav-ul-li-active')[i]).hasClass('active')) {
 				$($('.nav-ul-li-active')[i]).removeClass('active');
 			}
 		}
 	}
-	
-	$('.nav-ul-li').on('click', function() {
+
+	$('.nav-ul-li').on('click', function () {
 		removeClassActive();
 	})
-	
-	$('.nav-ul-li-active').on('click', function(ev) {
+
+	$('.nav-ul-li-active').on('click', function (ev) {
 		$(ev.target.parentNode).addClass('active');
 	});
 } ());
