10cd3555aeb2570d56ab0ce531f6259509ec81e8
Emil Nik <emil.nik1002@gmail.com>
2015-12-29 03:53:14 +0200

    Added data object

--- ToyLetters/app/common/data.js
+++ ToyLetters/app/common/data.js
@@ -1,2 +1,42 @@
-var data = function () {
-}();
+(function (){
+    function data($http, $q) {
+        
+        function get(url, params) {
+            var defered = $q.defer();
+
+            $http
+                .get(url, {
+                    params: params
+                })
+                .then(function (response) {
+                    defered.resolve(response.data);
+                }, function (err) {
+                    defered.reject(err.data);
+                });
+
+            return defered.promise;
+        }
+        
+        function post(url, data) {
+            var defered = $q.defer();
+
+            $http
+                .post(url, data)
+                .then(function (response) {
+                    defered.resolve(response.data);
+                }, function (err) {
+                    defered.reject(err.data);
+                });
+
+            return defered.promise;
+        }
+        
+        return {
+            get: get,
+            post: post
+        }
+    }
+    
+    angular.module('ToyLettersApp.Services')
+        .factory('data', ['$http', '$q', data]);
+}())
